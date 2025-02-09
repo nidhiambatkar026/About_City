330569dad2488500dcf55d12d543ace283be1cb0
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-09 23:09:11 +0530

    background image and text changed

--- src/herosection.js
+++ src/herosection.js
@@ -1,8 +1,10 @@
+import bgimage from "./images/banner.jpg";
+
 export default function HeroSection() {
     return (
         <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
             <img 
-                src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" 
+                src={bgimage}
                 alt="Background Image" 
                 className="absolute inset-0 -z-10 size-full object-cover object-right md:object-center"
             />
@@ -21,35 +23,30 @@ export default function HeroSection() {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:mx-0">
                     <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
-                        Work with us
+                    Welcome to Nagpur!
                     </h2>
-                    <p className="mt-8 text-lg font-medium text-pretty text-gray-300 sm:text-xl/8">
-                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. 
-                        Elit sunt amet fugiat veniam occaecat fugiat.
+                    <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+                    Explore the heart of India, where rich heritage meets modern charm.
+                    From the serene Ambazari Lake to the bustling markets, Nagpur has something for everyone.
                     </p>
                 </div>
                 <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-                        <a href="#">Open roles <span aria-hidden="true">&rarr;</span></a>
-                        <a href="#">Internship program <span aria-hidden="true">&rarr;</span></a>
-                        <a href="#">Our values <span aria-hidden="true">&rarr;</span></a>
-                        <a href="#">Meet our leadership <span aria-hidden="true">&rarr;</span></a>
-                    </div>
+                    
                     <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
                         <div className="flex flex-col-reverse gap-1">
-                            <dt className="text-base/7 text-gray-300">Offices worldwide</dt>
+                            <dt className="text-base/7 text-gray-300"> Historic Landmarks</dt>
                             <dd className="text-4xl font-semibold tracking-tight text-white">12</dd>
                         </div>
                         <div className="flex flex-col-reverse gap-1">
-                            <dt className="text-base/7 text-gray-300">Full-time colleagues</dt>
+                            <dt className="text-base/7 text-gray-300">Educational Institutions</dt>
                             <dd className="text-4xl font-semibold tracking-tight text-white">300+</dd>
                         </div>
                         <div className="flex flex-col-reverse gap-1">
-                            <dt className="text-base/7 text-gray-300">Hours per week</dt>
+                            <dt className="text-base/7 text-gray-300">Parks & Gardens</dt>
                             <dd className="text-4xl font-semibold tracking-tight text-white">40</dd>
                         </div>
                         <div className="flex flex-col-reverse gap-1">
-                            <dt className="text-base/7 text-gray-300">Paid time off</dt>
+                            <dt className="text-base/7 text-gray-300">Opportunities & Growth</dt>
                             <dd className="text-4xl font-semibold tracking-tight text-white">Unlimited</dd>
                         </div>
                     </dl>
