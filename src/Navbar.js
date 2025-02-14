89f59e151e21527299b9b7c935877a9b1aa4c945
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-14 13:24:32 +0530

    changes done in some components

--- src/Navbar.js
+++ src/Navbar.js
@@ -42,9 +42,6 @@ export default function Navbar() {
                     Home
                   </a>
                   <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-                    About
-                  </a>
-                  <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
                     Login
                   </a>
                   <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
@@ -106,9 +103,6 @@ export default function Navbar() {
           <div className="space-y-1 px-2 pt-2 pb-3">
             <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">
               Home
-            </a>
-            <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
-              About
             </a>
             <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
               Login
--- src/contact.js
+++ src/contact.js
@@ -15,10 +15,10 @@ export default function Contact() {
         </div>
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-            Contact sales
+            Contact Us
           </h2>
           <p className="mt-2 text-lg text-gray-600">
-            Aute magna irure deserunt veniam aliqua magna enim voluptate.
+            Connect with us for more details.
           </p>
         </div>
         <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
