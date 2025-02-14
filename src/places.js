4bda84260d86a16e3ea0fdcb427f4227385ba5fa
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-14 13:49:09 +0530

    changes done

--- src/places.js
+++ src/places.js
@@ -1,3 +1,12 @@
+import ambarzariLake  from "./images/ambazari-lake-nagpur.jpg";
+import ramtek from "./images/Ramtek.jpeg";
+import Sitabuldi from "./images/SitabuldiFort.webp";
+import Ramanscience from "./images/RamanScienceCenter.jpg";
+import dragonpalace from "./images/DragonPalaceTemple.avif";
+import Khindsi from "./images/KhindsiLake.jpg";
+import Futala from "./images/FutalaLake.jpg";
+import Umredwildlife from "./images/UmredKarhandlaWildlifeSanctuary.jpeg";
+
 export default function Places(){
     return(
         <div className="bg-white">
@@ -6,13 +15,69 @@ export default function Places(){
 
     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       <div className="group relative">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+        <img src={ambarzariLake} alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
         <div className="mt-4 flex justify-between">
           <div>
             <h3 className="text-sm text-gray-700">
               <a href="#">
                 <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
+                Ambarzari Lake
+              </a>
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="group relative">
+        <img src={ramtek} alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Ramtek
+              </a>
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="group relative">
+        <img src={Sitabuldi} alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Sitabuldi Fort
+              </a>
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="group relative">
+        <img src={Khindsi} alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                Khindsi Lake
+              </a>
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="group relative">
+        <img src={dragonpalace} alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+              <a href="#">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+               Dragon Palace Temple
               </a>
             </h3>
             <p className="mt-1 text-sm text-gray-500">Black</p>
@@ -22,13 +87,13 @@ export default function Places(){
       </div>
 
       <div className="group relative">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+        <img src={Ramanscience} alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
         <div className="mt-4 flex justify-between">
           <div>
             <h3 className="text-sm text-gray-700">
               <a href="#">
                 <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
+                Raman Science Center
               </a>
             </h3>
             <p className="mt-1 text-sm text-gray-500">Black</p>
@@ -38,98 +103,30 @@ export default function Places(){
       </div>
 
       <div className="group relative">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+        <img src={Futala} alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
         <div className="mt-4 flex justify-between">
           <div>
             <h3 className="text-sm text-gray-700">
               <a href="#">
                 <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
+               Futala Lake
               </a>
             </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
           </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
         </div>
       </div>
 
       <div className="group relative">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
+        <img src={Umredwildlife} alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
         <div className="mt-4 flex justify-between">
           <div>
             <h3 className="text-sm text-gray-700">
               <a href="#">
                 <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
+                Umred Karhandla Wild life Sanctuary
               </a>
             </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
           </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
-        </div>
-      </div>
-
-      <div className="group relative">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
-        </div>
-      </div>
-
-      <div className="group relative">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
-        </div>
-      </div>
-
-      <div className="group relative">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
-        </div>
-      </div>
-
-      <div className="group relative">
-        <img src="https://tailwindui.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"/>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
         </div>
       </div>
 
