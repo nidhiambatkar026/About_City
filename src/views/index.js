3525ed444797072abaa81c7ade7af35fa7311ef4
nidhiambatkar026 <nidhiambatkar@gmail.com>
2025-02-15 12:17:55 +0530

    folder structure updated

--- src/views/index.js
+++ src/views/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import HeroSection from "./herosection.js";
-import Navbar from "./navbar.js";
+import Navbar from "../components/navbar.js";
 import Contact from "./contact.js";
 import Login from "./login.js";
 import Places from "./places.js";
